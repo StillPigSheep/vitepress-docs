ab9575c1c563eccc85356320734388809b84b907
Stupidsheepy <2983626014@qq.com>
2023-11-20 13:19:01 +0800

    $(date)

---
+++ deploy_dist.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# cd 'D:\800Projects\Vitepress\.vitepress\dist'
+
+# git config --global user.name Stupidsheepy
+# git config --global user.email 2983626014@qq.com
+git add . 
+git commit -m '$(date)'
+git push -u origin main
